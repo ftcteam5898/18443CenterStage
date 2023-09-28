28ce29109be433478613ec414006d5947e5e036e
aedangill2008 <146353715+aedangill2008@users.noreply.github.com>
2023-09-28 09:47:47 -0400

    Update StraferFieldCentric.java
    Changed rev hub orientation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StraferFieldCentric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StraferFieldCentric.java
@@ -45,8 +45,8 @@ public class StraferFieldCentric extends LinearOpMode {
         imu = hardwareMap.get(IMU.class, "imu");
         // Adjust the orientation parameters to match your robot
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.FORWARD,
-                RevHubOrientationOnRobot.UsbFacingDirection.UP));
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD));
         // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
         imu.initialize(parameters);
 
